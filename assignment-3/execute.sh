779f46dc00da2a946c78d2064a429234462d0384
swaite <swaite@rubiconproject.com>
2017-10-18 17:48:28 -0600

    adding assignment 6

--- assignment-3/execute.sh
+++ assignment-3/execute.sh
@@ -3,4 +3,3 @@
 /opt2/spark-2.2.0-bin-hadoop2.7/bin/spark-submit \
 --properties-file config.conf \
 problem-6.py
-
---
+++ assignment-6/execute.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/opt/spark-2.2.0-bin-hadoop2.7/bin/spark-submit \
+network-count-structured.py
